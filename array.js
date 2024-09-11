8804b55afd1bb12f4b3d93cf1ff219d84cc1c6a9
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-09-12 02:18:22 +0600

    Solved Array problem

--- array.js
+++ array.js
@@ -1,4 +1,22 @@
-let testArray=[1,2,3,4,5,6];
-console.log(testArray);
-testArray.push[1];
-console.log(testArray);
+
+let testArray=[1,2,3,4,5,6,7,8,9];
+console.log("This is original array:",testArray);
+
+
+testArray.push(10);
+console.log("Push/Add new data to the last index of array:",testArray);
+
+
+testArray.pop();
+console.log("Pop/delete data from last index of array:",testArray);
+
+
+testArray.shift();
+console.log("Shift/delete data from starting index of array:",testArray);
+
+
+testArray.unshift(0);
+ console.log("Unshift/Push/Add data to starting index of array:",testArray);
+
+
+
